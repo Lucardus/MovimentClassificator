4b298b7ef33cc42538d4e27e36bcac1498f98b9e
Lucardus <lucaseduardoorlandoleo11@gmail.com>
2026-04-13 19:11:10 -0300

    fix PILImage

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pathlib
 pathlib.WindowsPath = pathlib.PosixPath
 
 from fastai.vision.all import *
-from PIL import ImageFile, Image
-import numpy as np
+from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import gradio as gr
+import numpy as np
 
 learn = load_learner('model.pkl')
 
@@ -13,13 +13,13 @@ categorias = ('pessoa agachada', 'pessoa andando', 'pessoa correndo',
               'pessoa deitada', 'pessoa parada', 'pessoa pulando', 'pessoa sentada')
 
 def classificar(img):
-    img = Image.fromarray(img.astype('uint8'), 'RGB')
+    img = PILImage.create(img)
     pred, idx, probs = learn.predict(img)
     return dict(zip(categorias, map(float, probs)))
 
 intf = gr.Interface(
     fn=classificar,
-    inputs=gr.Image(width=224, height=224),
+    inputs=gr.Image(),
     outputs=gr.Label(num_top_classes=3),
     title="Classificador de Movimentos Humanos",
     description="Envie uma foto e o modelo identifica o movimento da pessoa"
