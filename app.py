980a89569d5530337a1bc6f946bdf98155a271f6
Lucardus <lucaseduardoorlandoleo11@gmail.com>
2026-04-13 19:23:12 -0300

    fix tensor pipeline

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from PIL import ImageFile, Image
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 import gradio as gr
 import numpy as np
+import torch
 
 learn = load_learner('model.pkl')
 
@@ -13,10 +14,12 @@ categorias = ('pessoa agachada', 'pessoa andando', 'pessoa correndo',
               'pessoa deitada', 'pessoa parada', 'pessoa pulando', 'pessoa sentada')
 
 def classificar(img):
-    img_pil = Image.fromarray(np.uint8(img))
-    img_pil.save('/tmp/temp_img.jpg')
-    img_fastai = PILImage.create('/tmp/temp_img.jpg')
-    pred, idx, probs = learn.predict(img_fastai)
+    img_pil = Image.fromarray(np.uint8(img)).convert('RGB').resize((224, 224))
+    img_tensor = torch.tensor(np.array(img_pil)).permute(2, 0, 1).float() / 255.0
+    img_tensor = img_tensor.unsqueeze(0)
+    with torch.no_grad():
+        preds = learn.model(img_tensor)
+        probs = torch.softmax(preds[0], dim=0)
     return dict(zip(categorias, map(float, probs)))
 
 intf = gr.Interface(
